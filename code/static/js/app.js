cf2cc856636f7d3aab2693c3f4442e8c91ef7f5f
jared.roussel@outlook.com <jared.roussel@outlook.com>
2021-11-21 15:37:15 -0500

    Updated code

--- code/static/js/app.js
+++ code/static/js/app.js
@@ -9,12 +9,32 @@ d3.json("samples.json").then(function(data) {
 //Charts values
 d3.json("samples.json").then(function(data) {
   var samples = data.samples;
-  var otu_ids = sample.otu_ids;
-  var otu_labels = sample.otu_labels;
-  var values = sample.values;
+  var otu_ids = samples.otu_ids;
+  var otu_labels = samples.otu_labels;
+  var values = samples.values;
 
 });
 
+var bubbleLayout = {
+  margin: { t: 0 },
+  xaxis: { title: "OTU ID" },
+  hovermode: "closest",
+  };
 
+  //Convert to a list
+  var dataBubble = [ 
+  {
+    x: otu_ids,
+    y: values,
+    text: otu_labels,
+    mode: "markers",
+    marker: {
+      color: otu_ids,
+      size: values,
+      }
+  }
+];
+
+Plotly.newPlot("bubble", dataBubble, bubbleLayout);
 
   
